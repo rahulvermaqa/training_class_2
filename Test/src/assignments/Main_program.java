913556bf32a3a92a5c8da4818cbc154a78028603
rahulvermaqa <rahulverma@qainfotech.com>
2017-01-17 13:29:32 +0530

    Main Program to include different objects

--- Test/src/assignments/Main_program.java
+++ Test/src/assignments/Main_program.java
@@ -6,6 +6,20 @@ public class Main_program {
 	{
 		Try_catch_example E = new Try_catch_example();
 		E.take_input();
+		
+		Checked_unchecked_exceptions C = new Checked_unchecked_exceptions();
+		C.checked_exception();
+		C.unchecked_exception();
+		
+		Throw_example T = new Throw_example();
+		try
+		{
+			T.Throw_Arithmetic();
+		}
+		catch(ArithmeticException E)
+		{
+			System.out.println("You have entered 1 or Zero..Exception catched from my end");
+		}
 
 		
 	}
