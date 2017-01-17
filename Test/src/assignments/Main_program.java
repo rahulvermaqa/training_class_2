fdcf50602724d3b7c8b547936bf79864e996303e
rahulvermaqa <rahulverma@qainfotech.com>
2017-01-17 13:34:16 +0530

    Main Program

--- Test/src/assignments/Main_program.java
+++ Test/src/assignments/Main_program.java
@@ -1,26 +1,26 @@
-package assignments;
+	package assignments;
 
-public class Main_program {
+	public class Main_program {
 
-	public static void main(String args[])
-	{
-		Try_catch_example E = new Try_catch_example();
-		E.take_input();
-		
-		Checked_unchecked_exceptions C = new Checked_unchecked_exceptions();
-		C.checked_exception();
-		C.unchecked_exception();
-		
-		Throw_example T = new Throw_example();
-		try
+		public static void main(String args[])
 		{
-			T.Throw_Arithmetic();
-		}
-		catch(ArithmeticException Exception)
-		{
-			System.out.println("You have entered 1 or Zero..Exception catched from my end");
-		}
+			Try_catch_example E = new Try_catch_example();
+			E.take_input();
+			
+			Checked_unchecked_exceptions C = new Checked_unchecked_exceptions();
+			C.checked_exception();
+			C.unchecked_exception();
+			
+			Throw_example T = new Throw_example();
+			try
+			{
+				T.Throw_Arithmetic();
+			}
+			catch(ArithmeticException Exception)
+			{
+				System.out.println("You have entered 1 or Zero..Exception catched from my end");
+			}
 
-		
-	}
+			
+		}
 }
